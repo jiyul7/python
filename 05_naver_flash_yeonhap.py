22173279802b267830bf427ce273ba9e292fec43
leejiyul <68854426+jiyul7@users.noreply.github.com>
2020-08-11 21:45:54 +0900

    Update 05_naver_flash_yeonhap.py

--- 05_naver_flash_yeonhap.py
+++ 05_naver_flash_yeonhap.py
@@ -4,12 +4,26 @@ from requests.exceptions import HTTPError
 from pprint import pprint   # json data pretty format
 import bs4
 
-# url = "https://news.naver.com/main/list.nhn?mode=LPOD&sid2=140&sid1=001&mid=sec&oid=001&isYeonhapFlash=Y&date=20200806&page=1" 
-url = "https://news.naver.com/main/list.nhn?mode=LPOD&sid2=140&sid1=001&mid=sec&oid=001&isYeonhapFlash=Y"
+def getSoup(url):
+    res = requests.get(url)
+    return bs4.BeautifulSoup(res.text, "html.parser")
 
-res = requests.get(url)
-# print(res.text)
-soup = bs4.BeautifulSoup(res.text, "html.parser")
+def bodyNews(url):
+    url = "https://news.naver.com/main/list.nhn" + url
+    res = requests.get(url)
+    soup = bs4.BeautifulSoup(res.text, "html.parser")
+    pagebodynews = []
+    body = soup.find("ul", {"class":"type02"})
+    list = body.findAll("li", {"class":"_rcount"})
+
+    for li in list:
+        row = li.find("span", {"class":"date"}).text.strip() + ": " + li.find("a").text.strip()
+        pagebodynews.append(row)
+    return pagebodynews
+
+#######################################################################################################
+url = "https://news.naver.com/main/list.nhn?mode=LPOD&sid2=140&sid1=001&mid=sec&oid=001&isYeonhapFlash=Y"
+soup = getSoup(url)
 
 ################# Top news
 topnews = []
@@ -24,25 +38,25 @@ for one in topEtc:
 for a in topnews:
     print(a)
 
-################ Body news (get max page)
+################ Body news (get href + max page)
 limit = soup.find("div", {"class":"paging paging_ytn"})
 limit = limit.findAll("a")  # list안에 <a> </a>, 들만 쌓이면 find() 사용할 필요없이 바로 tag.text로 출력
-for b in limit:
-    # print(b.text)
-    max = b.text
+bodyNewsUrl = []
+for a in limit:
+    bodyNewsUrl.append(a['href'])
+    max = a.text
 print("MAX Page: " + max)
 
+firstPage = bodyNewsUrl[0]
+firstPage = firstPage.replace("page=2", "page=1")
+bodyNewsUrl.insert(0,firstPage)
+
 ################ Body news
-bodynews = []
-body = soup.find("ul", {"class":"type02"})
-list = body.findAll("li", {"class":"_rcount"})
+allBodyNews = []
 
-for li in list:
-    row = li.find("span", {"class":"date"}).text.strip() + ": " + li.find("a").text.strip()
-    bodynews.append(row)
+for url in bodyNewsUrl:
+    allBodyNews = allBodyNews + bodyNews(url)
 
-bodynews.sort(reverse=True)
-for a in bodynews:
+allBodyNews.sort(reverse=True)
+for a in allBodyNews:
     print(a)
-
-
